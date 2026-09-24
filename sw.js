4da62ea849643203b9bf8b05d677f6a6c96a520d
ibrahemE1 <138873761+ibrahemE1@users.noreply.github.com>
2026-09-24 11:17:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// عدّل رقم الإصدار عند كل تحديث للبيانات
+const CACHE = 'ehp-filters-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// أونلاين: يجلب أحدث نسخة ويحدّث التخزين. أوفلاين: يفتح النسخة المخزنة.
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET' || new URL(e.request.url).origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
